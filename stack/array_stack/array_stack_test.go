a51fa5130276efae6e464d4d20dd2ccd7f0c5f34
quzhen12 <quzhen12@gmail.com>
2020-04-07 23:26:53 +0800

    feat(array_stack_test): add test: stack overflow and stack underflow

--- stack/array_stack/array_stack_test.go
+++ stack/array_stack/array_stack_test.go
@@ -17,4 +17,27 @@ func TestNewStack(t *testing.T) {
 	}
 }
 
+func TestStackOverflow(t *testing.T) {
+	stack := array_stack.NewStack(2)
+	stack.Push(10)
+	stack.Push(120)
+	err := stack.Push(101)
+	if err == nil || err.Error() != "stack overflow"{
+		t.Error("without overflow")
+	}
+	t.Log(err)
+}
+
+func TestStackUnderflow(t *testing.T) {
+	stack := array_stack.NewStack(2)
+	stack.Push(10)
+
+	stack.Pop()
+	_, err := stack.Pop()
+	if err == nil || err.Error() != "stack underflow" {
+		t.Error("without underflow")
+	}
+	t.Log(err)
+}
+
 
