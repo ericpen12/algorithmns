753a9295aff1f4e7a0974753813271abeca32fb3
quzhen12 <quzhen12@gmail.com>
2020-04-07 23:26:09 +0800

    feat(array_stack): add error: stack overflow

--- stack/array_stack/array_stack.go
+++ stack/array_stack/array_stack.go
@@ -4,23 +4,30 @@ import "errors"
 
 type stack struct {
 	top   int
+	len   int
 	array *[]int
 }
 
 type stacker interface {
-	Push(n int)
+	Push(n int) error
 	Pop() (int, error)
 	IsEmpty() bool
 }
 
-func (s *stack) Push(n int) {
+func (s *stack) Push(n int) error {
 	s.top++
+
+	if s.top >= s.len {
+		return errors.New("stack overflow")
+	}
+
 	(*s.array)[s.top] = n
+	return nil
 }
 
 func (s *stack) Pop() (int, error) {
 	if s.IsEmpty() {
-		return -1, errors.New("underflow")
+		return -1, errors.New("stack underflow")
 	}
 	s.top--
 	return (*s.array)[s.top+1], nil
@@ -34,6 +41,7 @@ func NewStack(size int) stacker {
 	arr := make([]int, size)
 	return &stack{
 		top:   0,
+		len:   size,
 		array: &arr,
 	}
 }
