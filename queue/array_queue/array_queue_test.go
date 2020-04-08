18145c06270108e01e5659e8db8fb6e00356f0ea
quzhen12 <quzhen12@gmail.com>
2020-04-08 21:15:21 +0800

    feat(array_queue_test): add overflow test and underflow test

--- queue/array_queue/array_queue_test.go
+++ queue/array_queue/array_queue_test.go
@@ -7,13 +7,43 @@ import (
 
 func TestNewQueue(t *testing.T) {
 	queue := array_queue.NewQueue(20)
+
+	for i := 0; i < 10; i++ {
+		queue.Enqueue(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		v, err := queue.Dequeue()
+		if err != nil {
+			t.Error(err)
+		}
+		if v != i {
+			t.Errorf("the queue excepted is %d, but the actual is %d", i, v)
+		}
+	}
+}
+
+func TestQueueOverflow(t *testing.T) {
+	queue := array_queue.NewQueue(2)
 	queue.Enqueue(1)
 	queue.Enqueue(2)
-	queue.Enqueue(3)
-	queue.Enqueue(4)
+	err := queue.Enqueue(3)
+	if err == nil || err.Error() != "queue overflow" {
+		t.Error("without underflow")
+	}
+	t.Log(err)
+}
 
-	t.Log(queue.Dequeue())
-	t.Log(queue.Dequeue())
-	t.Log(queue.Dequeue())
-	t.Log(queue.Dequeue())
+func TestQueueUnderflow(t *testing.T) {
+	queue := array_queue.NewQueue(2)
+	queue.Enqueue(1)
+	queue.Enqueue(2)
+	queue.Dequeue()
+	queue.Dequeue()
+	_, err := queue.Dequeue()
+
+	if err == nil || err.Error() != "queue underflow" {
+		t.Error("without underflow")
+	}
+	t.Log(err)
 }
